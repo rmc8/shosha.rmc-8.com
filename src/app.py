ed0d9faccb1152821a33bd4f7c79e39f8bc04e7d
rmc-8.com <kmprmc@gmail.com>
2023-12-31 22:11:17 +0900

    [ADD] keep_alive

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ load_dotenv(verbose=True)
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
+# from .keep_alive import keep_alive
 from libs.bsky import BskyClient
 
 
@@ -14,6 +15,7 @@ PASSWORD = os.getenv("APPPASS")
 
 
 def main():
+    # keep_alive()
     bc = BskyClient(HANDLE, PASSWORD)
     bc.run()
 
