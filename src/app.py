2e18cb74045fc9485562433c0a552b10fa94859a
rmc-8.com <kmprmc@gmail.com>
2023-12-31 22:20:47 +0900

    PEP8 E402

--- src/app.py
+++ src/app.py
@@ -2,13 +2,11 @@ import os
 
 from dotenv import load_dotenv
 
+from libs.bsky import BskyClient
+
 load_dotenv(verbose=True)
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
-
-# from .keep_alive import keep_alive
-from libs.bsky import BskyClient
-
 
 HANDLE = os.getenv("HANDLE")
 PASSWORD = os.getenv("APPPASS")
